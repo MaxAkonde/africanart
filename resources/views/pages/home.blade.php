0b7053756d6501ff48769407a8cf5979715d9a1e
MaxAkonde <devdevmaster@gmail.com>
2022-10-21 12:14:12 +0100

    fix bug

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -91,7 +91,7 @@
         </div>
         <div class="row px-xl-5 pb-3">
             @foreach ($latest as $item)
-                <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-1">
                     <div class="card product-item mb-4" style="border:1px solid #f3f3f3;">
                         <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0"
                             style="box-shadow: 1px 1px 5px #ddd">
@@ -158,7 +158,7 @@
         </div>
         <div class="row px-xl-5 pb-3">
             @foreach ($latest as $item)
-                <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="col-lg-3 col-md-6 col-sm-6 pb-1">
                     <div class="card product-item border-0 mb-4">
                         <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
                             <img class="img-fluid w-100" style="width: 500px; height:350px;"
